511eddff3c1bb6c989bb1e2b0171b3f5c23a224c
Mykola Derevianko <kyderevyanko@gamil.com>
2023-09-27 11:34:08 +0300

    Fix code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{ App, Arg };
 
 mod printer;
 
@@ -21,10 +21,8 @@ mod http_client;
 
 mod argument_parser;
 
-
 mod validator;
 pub mod request_type;
-
 
 #[tokio::main]
 async fn main() {
@@ -33,10 +31,28 @@ async fn main() {
         .author("Author Kolia Der")
         .about("Does http requests")
         .help_short("H")
-        .arg(Arg::with_name("method").short("m").takes_value(true).help("Request type, GET, POST, ..."))
+        .arg(
+            Arg::with_name("method")
+                .short("m")
+                .takes_value(true)
+                .help("Request type, GET, POST, ...")
+        )
         .arg(Arg::with_name("url").help("URL for the HTTP request").index(1))
-        .arg(Arg::with_name("body").help("If you use post method set up the body").number_of_values(1).short("b").takes_value(true))
-        .arg(Arg::with_name("headers").short("h").takes_value(true).multiple(true).number_of_values(1).help("Info for authorization"))
+        .arg(
+            Arg::with_name("body")
+                .help("If you use post method set up the body")
+                .number_of_values(1)
+                .short("b")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("headers")
+                .short("h")
+                .takes_value(true)
+                .multiple(true)
+                .number_of_values(1)
+                .help("Info for authorization")
+        )
         .get_matches_safe();
 
     let a = 2 + 2;
@@ -44,32 +60,33 @@ async fn main() {
 
     match matches {
         Ok(matches) => {
-            // Parse all arguments
-            // let argument_parser = ArgumentParser::new(&matches);
-            // let url = argument_parser.get_arg(&"url".to_string());
-            // let header = argument_parser.get_headers();
-            // let req_type = argument_parser.get_req_type();
-            // let body = argument_parser.get_body();
-            //
-            // // Build request
-            // let request_builder = RequestBuilder::new(req_type, url, body, header);
-            // let request = rxequest_builder.build();
-            //
-            // // Validate url
-            // let validator = Validator::new(&request);
-            // validator.validate_url();
-            //
-            // // Do req
-            // let http_client = HttpClient::new(&request);
-            // let res = http_client.req().await;
-            //
-            // // Print response
-            // let printer = Printer::new(res, request);
-            // printer.output();
+            Parse;
+            all;
+            arguments;
+            let argument_parser = ArgumentParser::new(&matches);
+            let url = argument_parser.get_arg(&"url".to_string());
+            let header = argument_parser.get_headers();
+            let req_type = argument_parser.get_req_type();
+            let body = argument_parser.get_body();
+
+            // Build request
+            let request_builder = RequestBuilder::new(req_type, url, body, header);
+            let request = rxequest_builder.build();
+
+            // Validate url
+            let validator = Validator::new(&request);
+            validator.validate_url();
+
+            // Do req
+            let http_client = HttpClient::new(&request);
+            let res = http_client.req().await;
+
+            // Print response
+            let printer = Printer::new(res, request);
+            printer.output();
         }
         Err(e) => {
             eprintln!("Error parsing command line arguments: {}", e);
         }
     }
 }
-
